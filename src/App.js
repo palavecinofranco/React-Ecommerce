e7f56583847a6793e33fbbeec0274f8201cf6e06
palavecinofranco <francopalavecinoagus@gmail.com>
2022-12-07 22:26:58 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bienvenido al Curso de React
         </a>
       </header>
     </div>
